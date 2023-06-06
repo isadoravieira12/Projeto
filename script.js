2496b32b1726f4126ff58a8500954fb621252941
@isadoravieira12 <isadoravieira232@gmail.com>
2023-06-06 11:37:01 -0300

    add alterações

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function toggleMode() {
   const html = document.documentElement
-
+  html.classList.toggle("light")
   //if(html.classList.contains('light')) {
   // html.classList.remove('light')
   //} else {
   //  html.classList.add('light')
   //}
-  html.classList.toggle("light")
+ 
 
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
   //substituir a imagem
   if (html.classList.contains("light")) {
-    img.setAttribute("scr", "avatar-light.png")
+    img.setAttribute("scr", "./assets/avatar-light.svg")
   } else {
     //set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "avatar.png")
+    img.setAttribute("src", "./assets/avatar.svg")
   }
 }
